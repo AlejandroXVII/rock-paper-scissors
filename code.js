1a55edda401ee2c96d2428cdbfabf5b0b7ab369f
AlejandroXVII <m.alejandro.xvii@gmail.com>
2023-11-13 18:44:05 -0400

    Add a function to chose the winner and pc to chose

--- code.js
+++ code.js
@@ -1,1 +1,45 @@
-console.log("HELLO WORLD")
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+  
+function getComputerChoice(){
+    n=getRandomInt(3);
+    if(n===0)
+        return "rock";
+    if(n===1)
+        return "paper"
+    if(n===2)
+        return "scissors"
+}
+
+function youWin(){
+    return "you have won!"
+}
+
+function youLose(){
+    return "you have lose:("
+}
+ 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection===computerSelection)
+        console.log("is a tie ")   
+    else {
+        if (playerSelection==="rock"){
+            if (computerSelection==="scissors")
+                console.log(youWin()) 
+            else console.log(youLose()) 
+        }
+        if (playerSelection==="paper"){
+            if (computerSelection==="rock")
+                console.log(youWin())
+            else console.log(youLose()) 
+        }
+        if (playerSelection==="scissors"){
+            if (computerSelection==="paper")
+                console.log(youWin())
+            else console.log(youLose()) 
+        }
+    }
+}
+
+console.log(getComputerChoice());
