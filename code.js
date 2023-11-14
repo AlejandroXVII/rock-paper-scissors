0e6cd5ec4da41c1ed14f9c1e4dd84c40114d6ce4
AlejandroXVII <m.alejandro.xvii@gmail.com>
2023-11-13 20:37:32 -0400

    Add game function and a Loop to play 5 times     Now you can play 5 times a then it chose the winner, or tell your     if no one won

--- code.js
+++ code.js
@@ -1,3 +1,8 @@
+
+let ties = 0;
+let computerWins = 0;
+let playerWins = 0;
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
@@ -13,16 +18,20 @@ function getComputerChoice(){
 }
 
 function youWin(){
+    playerWins = playerWins + 1;
     return "you have won!";
 }
 
 function youLose(){
-    return "you have lose:(";
+    computerWins = computerWins + 1;
+    return "you have lose!";
 }
  
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection===computerSelection)
+    if (playerSelection===computerSelection){
+        ties = ties + 1;
         return "is a tie ";
+    }
     else {
         if (playerSelection==="rock"){
             if (computerSelection==="scissors")
@@ -41,11 +50,31 @@ function playRound(playerSelection, computerSelection) {
         }
     }
 }
-let input = window.prompt("Rock! Paper! Scissors! CHOSE!:");
-input = input.toLowerCase()
-let computerInput = getComputerChoice();
-let result = playRound(input,computerInput);
-if (( input !== "rock" ) & (input !== "paper" ) & (input !== "scissors" )) console.log("ERROR!");
-else {
-    console.log("The computer has select ",computerInput," and you ",input," so ",result);
+
+function playOneRound(){
+    let input = window.prompt("Rock! Paper! Scissors! CHOSE!:");
+    input = input.toLowerCase()
+    let computerInput = getComputerChoice();
+    let result = playRound(input,computerInput);
+    if (( input !== "rock" ) & (input !== "paper" ) & (input !== "scissors" )) console.log("ERROR!");
+    else {
+        console.log("The computer has select ",computerInput," and you ",input," so ",result);
+    }
 }
+
+function game(){
+    for (let i = 0; i < 5; i++) {
+        playOneRound();
+    }
+    console.log("SCORE:\n Ties: ",ties," Player wins: ",playerWins," Computer wins: ",computerWins);
+    if (computerWins >= playerWins & computerWins >= ties)
+        console.log("The COMPUTER has been the winner, with",computerWins," victories.");
+    else{
+        if (computerWins <= playerWins & playerWins >= ties)
+            console.log("The PLAYER has been the winner, with",playerWins," victories.");
+        else
+            console.log("No one has win, this is a tie!");
+    }
+}
+
+game(); 
